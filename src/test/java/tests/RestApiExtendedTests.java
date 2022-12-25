c37c5136a2190b044ae84e58f146d809b38e5ec7
ArmanSakurov <a.sakurov@skyeng.ru>
2022-12-25 21:35:54 +0500

    [models] added variables

--- src/test/java/tests/RestApiExtendedTests.java
+++ src/test/java/tests/RestApiExtendedTests.java
@@ -12,38 +12,34 @@ import org.junit.jupiter.api.Test;
 import static helpers.CustomApiListener.withCustomTemplates;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
-import static org.hamcrest.CoreMatchers.hasItems;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static specs.CreateUserSpecs.createUserRequestSpec;
 import static specs.CreateUserSpecs.createUserResponseSpec;
 
 public class RestApiExtendedTests {
-//    Faker faker = new Faker();
-//    String name = faker.name().firstName();
-//    String job = faker.job().position();
-//    String email = faker.internet().emailAddress();
-//    String password = faker.number().randomNumber() + name;
-//
-//
-//    @Test
-//    public void postCreateUserTest(){
-//        given().log().all().
-//                body("{\"name\": \"" + name + "\",\"job\": \"" + job + "\"}")
-//                .contentType(JSON)
-//                .when()
-//                .post("https://reqres.in/api/users")
-//                .then()
-//                .statusCode(201)
-//                .assertThat().body("name", is(name))
-//                .assertThat().body("job", is(job));
-//    }
+    Faker faker = new Faker();
+    String name = faker.name().firstName();
+    String job = faker.job().position();
+
+
+    @Test
+    public void postCreateUserTest(){
+        given().log().all().
+                body("{\"name\": \"" + name + "\",\"job\": \"" + job + "\"}")
+                .contentType(JSON)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .statusCode(201)
+                .assertThat().body("name", is(name))
+                .assertThat().body("job", is(job));
+    }
 
     @Test
     public void createUserWithPojoModelTest(){
         CreateUserModel body = new CreateUserModel();
-        body.setName("testName");
-        body.setJob("testJob");
+        body.setName(name);
+        body.setJob(job);
 
         CreateUserResponseModel response =
                 given()
@@ -57,15 +53,15 @@ public class RestApiExtendedTests {
                         .statusCode(201)
                         .extract().as(CreateUserResponseModel.class);
 
-        Assertions.assertThat(response.getName()).isEqualTo("testName");
-        Assertions.assertThat(response.getJob()).isEqualTo("testJob");
+        Assertions.assertThat(response.getName()).isEqualTo(name);
+        Assertions.assertThat(response.getJob()).isEqualTo(job);
     }
 
     @Test
     void createUserWithLombokTest() {
         CreateUserLombokModel body = new CreateUserLombokModel();
-        body.setName("testName");
-        body.setJob("testJob");
+        body.setName(name);
+        body.setJob(job);
 
         CreateUserResponseLombokModel response = given()
                 .filter(withCustomTemplates()) //with Custom Allure Listener
@@ -80,15 +76,15 @@ public class RestApiExtendedTests {
                 .statusCode(201)
                 .extract().as(CreateUserResponseLombokModel.class);
 
-        Assertions.assertThat(response.getName()).isEqualTo("testName");
-        Assertions.assertThat(response.getJob()).isEqualTo("testJob");
+        Assertions.assertThat(response.getName()).isEqualTo(name);
+        Assertions.assertThat(response.getJob()).isEqualTo(job);
     }
 
     @Test
     void createUserWithSpecsTest() {
         CreateUserLombokModel body = new CreateUserLombokModel();
-        body.setName("testName");
-        body.setJob("testJob");
+        body.setName(name);
+        body.setJob(job);
 
         CreateUserLombokModel response = given() //given(createUserRequestSpec)
                 .spec(createUserRequestSpec)
@@ -99,7 +95,7 @@ public class RestApiExtendedTests {
                 .spec(createUserResponseSpec)
                 .extract().as(CreateUserLombokModel.class);
 
-        Assertions.assertThat(response.getName()).isEqualTo("testName");
-        Assertions.assertThat(response.getJob()).isEqualTo("testJob");
+        Assertions.assertThat(response.getName()).isEqualTo(name);
+        Assertions.assertThat(response.getJob()).isEqualTo(job);
     }
 }
